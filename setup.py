71d249668ae88730cbc606c7e71abaf50e9212e0
Philipp von Weitershausen <philipp@weitershausen.de>
2008-01-06 19:24:03 +0000

    Adjust version number and externals to match 2.11.0b1 release.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='ExtensionClass',
-      version = '2.11.0dev',
+      version = '2.11.0b1',
       url='http://cheeseshop.python.org/pypi/ExtensionClass',
       license='ZPL 2.1',
       description='Metaclass for subclassable extension types',
