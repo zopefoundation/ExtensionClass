7213312896e79ba5309e3194f53da691e208eefd
David Glick <david@glicksoftware.com>
2013-03-20 13:09:45 -0700

    add python reference impl of MethodObject and ComputedAttribute

--- src/MethodObject/__init__.py
+++ src/MethodObject/__init__.py
@@ -1,1 +1,24 @@
-from _MethodObject import *
+import new
+from ExtensionClass import Base
+
+
+class Method(Base):
+    """Base class for objects that want to be treated as methods
+
+    The method class provides a method, __of__, that
+    binds an object to an instance.  If a method is a subobject
+    of an extension-class instance, the the method will be bound
+    to the instance and when the resulting object is called, it
+    will call the method and pass the instance in addition to
+    other arguments.  It is the responsibility of Method objects
+    to implement (or inherit) a __call__ method.
+    """
+
+    def __of__(self, inst):
+        return new.instancemethod(self, inst)
+
+
+try:
+    from _MethodObject import *
+except:
+    pass
