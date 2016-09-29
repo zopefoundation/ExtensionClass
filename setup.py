d83a96e09e3c5d6f4ae0b0187ca3f53cf3cff7fa
Tres Seaver <tseaver@palladion.com>
2016-09-29 10:34:28 -0400

    Bump minor version.
    Changes to supported Python versions aren't bugfixes.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name='ExtensionClass',
-    version='4.1.3.dev0',
+    version='4.2.0.dev0',
     url='http://pypi.python.org/pypi/ExtensionClass',
     license='ZPL 2.1',
     description='Metaclass for subclassable extension types',
