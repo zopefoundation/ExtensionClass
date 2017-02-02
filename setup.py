6a244d289f502624f73fae504b73cb44296e1eed
Hanno Schlichting <hanno@hannosch.eu>
2017-02-02 13:45:12 +0100

    vb [ci skip]

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ else:
 
 setup(
     name='ExtensionClass',
-    version='4.2.1',
+    version='4.2.2.dev0',
     url='http://pypi.python.org/pypi/ExtensionClass',
     license='ZPL 2.1',
     description='Metaclass for subclassable extension types',
